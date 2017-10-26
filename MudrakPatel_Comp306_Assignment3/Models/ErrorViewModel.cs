0fdbaa6da7c7435607b8b281abef35fde4f23517
mudrakpatel <mudrakpatel96@gmail.com>
2017-10-26 11:12:03 -0400

    Add project files.

---
+++ MudrakPatel_Comp306_Assignment3/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MudrakPatel_Comp306_Assignment3.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
